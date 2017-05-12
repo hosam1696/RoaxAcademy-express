ef55e615439dac576b0011c712ba5da8bce962c3
hosamsam <hosamelnabawy1996@gmail.com>
2017-05-12 12:23:42 +0200

    #3 Fix data.json file

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,11 +1,9 @@
 let express = require('express');
 let router  = express.Router();
-let data    = require("../data/data.json");
+
 let feedback    = require("../data/feedback.json");
 
-router.get('/api/data', (request, response)=>{
-	response.json(data);
-});
+
 router.get('/api/feedback', (request, response)=>{
 	response.json(feedback);
 });
